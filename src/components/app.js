c37ece741aa2134a764073ec9c39df532abe4526
Strupinskiy <IStrupin@russellinvestments.com>
2017-06-22 08:14:38 -0700

    wired up book list detail

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list.jsx';
+import BookDetail from '../containers/book-detail.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
