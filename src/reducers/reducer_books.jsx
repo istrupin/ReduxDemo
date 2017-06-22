05a8c92f26da9d8dbc0f3b4866f29bb9d4f400d6
Strupinskiy <IStrupin@russellinvestments.com>
2017-06-22 08:16:43 -0700

    added pages property

--- src/reducers/reducer_books.jsx
+++ src/reducers/reducer_books.jsx
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript' },
-        { title: 'Ruby' },
-        { title: 'Python'}, 
-        { title: 'Net'}
+        { title: 'Javascript', pages: 101 },
+        { title: 'Ruby', pages: 1},
+        { title: 'Python', pages: 85}, 
+        { title: 'Net', pages: 39}
     ]
 }
